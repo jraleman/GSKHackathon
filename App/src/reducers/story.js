bb3a6f6c84d8556572d5cb6a2c1c96404e831b93
Felipe Romo <felipe@fromo.me>
2018-07-14 15:03:31 -0500

    reducer now handles dummy load_story action

--- App/src/reducers/story.js
+++ App/src/reducers/story.js
@@ -1,11 +1,18 @@
+import { LOAD_STORY } from '../actions/constants';
+
 const defaultState = {
-  storyTitles: '',
+  storyTitle: '',
   pages: [],
   currentPage: undefined
 };
 
 const story = (state = defaultState, action) => {
   switch (action.type) {
+    case LOAD_STORY:
+      return {
+        ...state,
+        pages: action.payload
+      };
     default:
       return state;
   }
