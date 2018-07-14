607be5e642a71426d83135f71fe8ccd04f2b6558
jraleman <jraleman@bendiburg.com>
2018-07-13 22:18:53 -0700

    Add dummy RootContainer

---
+++ App/src/containers/RootContainer/RootContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RootContainer extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <div className={'root-container'}>
+      </div>
+    );
+  }
+}
