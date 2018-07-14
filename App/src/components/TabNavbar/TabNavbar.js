452300518e9165c8a611a5a00de52c6b93a0bed2
jraleman <jraleman@bendiburg.com>
2018-07-14 10:46:01 -0700

    Refactor TabNavbar

--- App/src/components/TabNavbar/TabNavbar.js
+++ App/src/components/TabNavbar/TabNavbar.js
@@ -6,36 +6,42 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import styles from './styles';
 
-const styles = {
-  root: {
-    width: 500,
-  },
-};
 
 class TabNavbar extends React.Component {
-  state = {
-    value: 0,
-  };
-
+  constructor (props) {
+    super(props);
+    this._classes = this.props.classes;
+    this.state = {
+      value: 0
+    };
+    return ;
+  }
   handleChange = (event, value) => {
     this.setState({ value });
+    return ;
   };
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-
+  render () {
     return (
       <BottomNavigation
-        value={value}
-        onChange={this.handleChange}
-        showLabels
-        className={classes.root}
+        value={ this.state.value }
+        onChange={ this.handleChange }
+        showLabels={ true }
+        className={ this._classes.root }
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction
+          label="Recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          icon={<LocationOnIcon />}
+        />
       </BottomNavigation>
     );
   }
