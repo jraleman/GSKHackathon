6561082880c89ad6906440ec73b5a7b3e63f68bd
Felipe Romo <felipe@fromo.me>
2018-07-14 14:05:54 -0500

    refactors rootcontainer from class to functional component

--- App/src/containers/RootContainer/RootContainer.js
+++ App/src/containers/RootContainer/RootContainer.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import './styles.css';
-import logo from '../../images/logo.svg';
 import Story from '../../components/Story';
 
-class RootContainer extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {};
-    return ;
-  }
-  render () {
-    return (
-      <div className={'root-container'}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <Story />
-      </div>
-    );
-  }
-}
+const RootContainer = () => (
+  <Story />
+);
 
 export default RootContainer;
