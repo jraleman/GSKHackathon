ab37549e1534531b87d205c479c56184bacfd126
jraleman <jraleman@bendiburg.com>
2018-07-14 10:45:40 -0700

    Refactor AppHeader

--- App/src/components/AppHeader/AppHeader.js
+++ App/src/components/AppHeader/AppHeader.js
@@ -4,26 +4,35 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import styles from './styles';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-};
-
-function AppHeader(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            Title
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class AppHeader extends React.Component {
+  constructor (props) {
+    super(props);
+    this._classes = this.props.classes;
+    this._title = this.props.title;
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <div className={ this._classes.root }>
+        <AppBar
+          position={ "static" }
+          color={ "default" }
+        >
+          <Toolbar>
+            <Typography
+              variant={ "title" }
+              color={ "inherit" }
+            >
+              { this._title }
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 AppHeader.propTypes = {
