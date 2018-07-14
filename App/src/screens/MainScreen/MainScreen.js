b408a0718695199a6ed8db232744eb05b9c95aa2
jraleman <jraleman@bendiburg.com>
2018-07-14 00:02:07 -0700

    Add dummy MainScreen

---
+++ App/src/screens/MainScreen/MainScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MainScreen extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <div id={ 'MainScreen' }>
+
+      </div>
+    );
+  }
+}
+
+export default MainScreen;
