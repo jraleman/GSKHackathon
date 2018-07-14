59e8346dd63caa0c8644d2237f5158781be81131
jraleman <jraleman@bendiburg.com>
2018-07-14 11:49:09 -0700

    Refactor ComicScreen

--- App/src/containers/ComicScreen/ComicScreen.js
+++ App/src/containers/ComicScreen/ComicScreen.js
@@ -1,33 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Masonry from 'react-masonry-component';
 import ComicFilterButton from '../../components/ComicFilterButton';
 import ComicAutoSuggest from '../../components/ComicAutoSuggest';
 import ComicModalDialog from '../../components/ComicModalDialog';
 import ComicGridItem from '../../components/ComicGridItem';
+import styles from './styles';
 
 class ComicScreen extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this._classes = this.props.classes;
+    this.state = {
+      toggle: false
+    };
+    return ;
+  }
+  toggleModal = () => {
+    if (this.state.toggle == false) {
+      this.setState({ toggle: true });
+    }
+    else {
+      this.setState({ toggle: false });
+    }
+    return ;
+  }
+  navigateStory = () => {
+    alert('navigateStory');
     return ;
   }
   render () {
     return (
-      <React.Fragment>
-        <div>
-
-          <ComicFilterButton
-            text={ "I AM A BUTTON" }
-            disable={ false }
-            category={ 'option1' }
+      <div className={this._classes.root}>
+        <Grid
+          container={ true }
+          spacing={ 24 }
+        >
+          <ComicGridItem
+            title={ "ComicBook1" }
+            image={'https://www.pcgamesn.com/sites/default/files/gabe%20newell%20valve%20pc%20console.jpg'}
+            onPress={ this.toggleModal }
           />
-          <ComicAutoSuggest />
-          <ComicModalDialog />
-          <ComicGridItem />
-
-        </div>
-      </React.Fragment>
+          { this.state.toggle == false ? null :
+            <ComicModalDialog
+              title={ "ComicBook1" }
+              description={ "..." }
+              onPress={ this.navigateStory }
+            />
+          }
+        </Grid>
+      </div>
     );
   }
 }
 
-export default ComicScreen;
+ComicScreen.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ComicScreen);
---
+++ App/src/containers/ComicScreen/styles.js
@@ -0,0 +1,14 @@
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    paddingTop: 12,
+    paddingBottom: 12
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+export default styles;
