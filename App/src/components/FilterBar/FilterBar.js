e8885d3c9efed035c377d99bfab5ba452a4fe69a
jraleman <jraleman@bendiburg.com>
2018-07-14 00:50:17 -0700

    Add dummy FilterBar

---
+++ App/src/components/FilterBar/FilterBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class FilterBar extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <div className={ 'Filter-bar' }>
+        { "FilterComplete" }
+      </div>
+    );
+  }
+}
