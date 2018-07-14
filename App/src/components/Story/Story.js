4d883002b7c1aaf953929322534b3af3eabca59c
Felipe Romo <felipe@fromo.me>
2018-07-14 05:02:08 -0500

    PageViewer component and display pageId

--- App/src/components/Story/Story.js
+++ App/src/components/Story/Story.js
@@ -7,18 +7,43 @@ import StoryPage from '../StoryPage';
 export default class Story extends Component {
   constructor (props) {
     super(props);
+    this.state = {
+      pageId: 1
+    };
+    return ;
+  }
+
+  render () {
+    // will be all of the pages needed to tell the story
+    const pages = [];
+    return (
+      <Fragment>
+        <CssBaseline />
+        {/* this redirects back to the library view */}
+        <StoryCloseButton />
+        <PageViewer pageId={1} pages={pages} />
+        {/* <StoryPage /> */}
+        <StoryNavigation />
+      </Fragment>
+    );
+  }
+}
+
+class PageViewer extends Component {
+  constructor (props) {
+    super(props);
+    this.pages = this.props.pages;
+    this.pageId = this.props.pageId;
     this.state = {};
     return ;
   }
 
   render () {
+    const pageId = this.props.pageId ? this.props.pageId : 'failed to load page id';
     return (
-      <Fragment>
-        <CssBaseline />
-        <StoryCloseButton />
-        <StoryPage />
-        <StoryNavigation />
-      </Fragment>
+      <div>
+        { `Now displaying pageId: ${pageId}` }
+      </div>
     );
   }
 }
