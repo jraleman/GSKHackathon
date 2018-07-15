f5bcd75efa77e530c612d3806fe2014dcf3ec79b
jraleman <jraleman@bendiburg.com>
2018-07-15 04:59:59 -0700

    Add linkNavigation paths structure

--- App/src/containers/RootContainer/RootContainer.js
+++ App/src/containers/RootContainer/RootContainer.js
@@ -3,25 +3,30 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import AppHeader from '../../components/AppHeader';
 import TabNavbar from '../../components/TabNavbar';
-import LibraryPageViewer from '../LibraryPageViewer';
 import themes from '../../themes';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
 const logoAlt = require('../../themes/images/logo-alt.svg');
+const linkNavigation = {
+  landing: '/library',
+  body: '/story',
+  about: '/info'
+}
 
 const RootContainer = () => (
   <React.Fragment>
-    <MuiThemeProvider theme={themes}>
+    <MuiThemeProvider theme={ themes }>
       <AppHeader
-        logo={logoAlt}
-        title={'Narwell :: Just a pinch'}
+        logo={ logoAlt }
+        title={ 'Narwell :: Just a pinch' }
       />
       <CssBaseline />
-      <Link to='/story'>
+      {/* <Link to='/story'> */}
+      <Link to={ linkNavigation } >
         StoryBook
       </Link>
-      <LibraryPageViewer />
+      <InfoPageViewer />
       <TabNavbar />
     </MuiThemeProvider>
   </React.Fragment>
