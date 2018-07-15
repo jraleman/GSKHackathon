1a9ca464579d7c7e9b42d854874034be843fa2db
Felipe Romo <felipe@fromo.me>
2018-07-15 02:34:42 -0500

    Fixes display bug when no storybook loaded

--- App/src/components/Story/Story.js
+++ App/src/components/Story/Story.js
@@ -3,15 +3,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import StoryNavigation from '../../containers/StoryNavigation';
 import StoryCloseButton from '../StoryCloseButton';
 import StoryPageViewer from '../../containers/StoryPageViewer';
+import { Redirect } from 'react-router-dom';
 
 const Story = ({ story }) => (
+  story.storyPages.length ? (
   <Fragment>
     <CssBaseline />
     <StoryCloseButton />
     <StoryPageViewer page={story.storyPages.find(page => page.pageId == story.currentPage)}
     />
     <StoryNavigation />
-  </Fragment>
+  </Fragment>) : <Redirect to='/' />
 );
 
 export default Story;
