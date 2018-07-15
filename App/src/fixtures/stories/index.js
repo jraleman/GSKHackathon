b43f44da30a02dd32353edeb7b8ceb0542f2e013
jraleman <jraleman@bendiburg.com>
2018-07-15 12:31:36 -0700

    Add more placeholder data :)

--- App/src/fixtures/stories/index.js
+++ App/src/fixtures/stories/index.js
@@ -1,6 +1,8 @@
 const storiesList = [
   require("./json/storyOne.json"),
-  require("./json/storyTwo.json")
+  require("./json/storyTwo.json"),
+  require("./json/storyThree.json"),
+  require("./json/storyFour.json"),
 ];
 
 export default storiesList;
