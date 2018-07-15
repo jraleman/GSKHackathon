f73bcc16db9629befda1e321e089c09e2c7f979c
zedin27 <zeidtisnes@gmail.com>
2018-07-15 13:06:12 -0700

    more storybooks

--- App/src/fixtures/stories/index.js
+++ App/src/fixtures/stories/index.js
@@ -3,6 +3,12 @@ const storiesList = [
   require("./json/storyTwo.json"),
   require("./json/storyThree.json"),
   require("./json/storyFour.json"),
+  require("./json/storyFive.json"),
+  require("./json/storySix.json"),
+  require("./json/storySeven.json"),
+  require("./json/storyEight.json"),
+  require("./json/storyNine.json"),
+  require("./json/storyTen.json"),
 ];
 
 export default storiesList;
