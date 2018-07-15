4bfc523de9e7298456ea1a0b1318a497a9fe4157
Felipe Romo <felipe@fromo.me>
2018-07-14 21:07:57 -0500

    display story info

--- App/src/components/StoryPageViewer/StoryPageViewer.js
+++ App/src/components/StoryPageViewer/StoryPageViewer.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 const StoryPageViewer = ({ story }) => (
   <div>
+    { `StoryId: ${story.storyId}` }
+    <hr />
+    { `StoryTitle: ${story.storyTitle}` }
+    <hr />
+    { `StoryAuthor: ${story.storyAuthor}` }
+    <hr />
+    { `StoryCover: ${story.storyCover}` }
+    <hr />
+    { `StoryDescription: ${story.story}` }
+    <hr />
     { `Now displaying pageId: ${story.currentPage}` }
   </div>
 );
