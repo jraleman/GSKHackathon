f40a668baa23ab298718562235c56eb7aa1dea34
jraleman <jraleman@bendiburg.com>
2018-07-14 17:00:06 -0700

    Update theme colors

--- App/src/themes/index.js
+++ App/src/themes/index.js
@@ -1,19 +1,28 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-
 export default createMuiTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
+      neutral: '#a7bbc2'
+      light: '#55c2bc',
+      main: '#22aad0',
+      dark: '#094761',
       contrastText: '#fff',
     },
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
+      neutral: '#c292bb'
+      light: '#8e3482',
+      main: '#671e5c',
+      dark: '#390e34',
       contrastText: '#000',
     },
+    contrast: {
+      dark: '#121212',
+      light: '#f9f9f9'
+    }
+    success: '#b8e986',
+    danger: '#d9534f',
+    warning: '#f3bd71',
+    info: '#92bced',
   },
 });
