aa149794f5be9fa620efd69ac6df994fb0d17cea
Felipe Romo <felipe@fromo.me>
2018-07-15 11:38:23 -0500

    removes commented out code

--- App/src/components/StoryNavigation/StoryNavigation.js
+++ App/src/components/StoryNavigation/StoryNavigation.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import StoryNavigationButton from '../StoryNavigationButton';
 
-// export default class StoryNavigation extends Component {
-//   constructor (props) {
-//     super(props);
-//     this.state = {};
-//     return ;
-//   }
-
-//   render () {
-//     return (
-//       <div>
-//         <StoryNavigationButton
-//           direction='left'
-//           pageIdFunc={this.props.pageIdDec}
-//         />
-//         <StoryNavigationButton
-//           direction='right'
-//           pageIdFunc={this.props.pageIdInc}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 const StoryNavigation = ({ pageIdInc, pageIdDec, pageId }) => (
   <div>
     <StoryNavigationButton
