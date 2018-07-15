ec2793699847a00cfbf5fce4ceed6f4fbd671fba
jraleman <jraleman@bendiburg.com>
2018-07-15 12:37:13 -0700

    Improve InfoPageViewer style

--- App/src/containers/InfoPageViewer/styles.js
+++ App/src/containers/InfoPageViewer/styles.js
@@ -1,15 +1,23 @@
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    paddingTop: 12,
-    paddingBottom: 12,
+    // paddingTop: 24,
+    // paddingBottom: 24,
     marginLeft: 40,
     marginRight: 40
+  },
+  divisor: {
+    paddingTop: 12,
+    paddingBottom: 12,
   },
   paper: {
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    marginLeft: 40,
+    marginRight: 40
+    // paddingTop: 24,
+    // paddingBottom: 24,
   },
   menuButton: {
     marginLeft: -12,
