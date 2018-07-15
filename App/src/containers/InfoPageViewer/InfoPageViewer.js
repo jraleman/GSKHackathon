503d1054ddfe343f5fd919199929ca3c2b784640
jraleman <jraleman@bendiburg.com>
2018-07-15 04:07:12 -0700

    Add InfoPageViewer styles

--- App/src/containers/InfoPageViewer/InfoPageViewer.js
+++ App/src/containers/InfoPageViewer/InfoPageViewer.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import styles from './styles';
 
 class InfoPageViewer extends React.Component {
   constructor (props) {
     super(props);
+    this._classes = this.props.classes;
     this.state = {};
     return ;
   }
   render () {
     return (
-      <div className={this._classes.root}>
+      <div className={ this._classes.root }>
+        <div style={{ textAlign: 'center' }}>
+          <p>{ "InfoPageViewer" }</p>
+        </div>
       </div>
     );
   }
