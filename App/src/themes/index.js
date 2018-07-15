a6b6796cddf1a5252f1f26d0eedd2a22eceddc95
jraleman <jraleman@bendiburg.com>
2018-07-14 17:06:47 -0700

    Fix colors declaration

--- App/src/themes/index.js
+++ App/src/themes/index.js
@@ -3,14 +3,14 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
   palette: {
     primary: {
-      neutral: '#a7bbc2'
+      neutral: '#a7bbc2',
       light: '#55c2bc',
       main: '#22aad0',
       dark: '#094761',
       contrastText: '#fff',
     },
     secondary: {
-      neutral: '#c292bb'
+      neutral: '#c292bb',
       light: '#8e3482',
       main: '#671e5c',
       dark: '#390e34',
@@ -19,7 +19,7 @@ export default createMuiTheme({
     contrast: {
       dark: '#121212',
       light: '#f9f9f9'
-    }
+    },
     success: '#b8e986',
     danger: '#d9534f',
     warning: '#f3bd71',
