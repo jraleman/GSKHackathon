eab901910f06f89b8558f6848cf0826be3df8f15
Felipe Romo <felipe@fromo.me>
2018-07-15 15:15:28 -0500

    empty commit

--- App/src/fixtures/stories/index.js
+++ App/src/fixtures/stories/index.js
@@ -1,8 +1,4 @@
 const storiesList = [
-<<<<<<< HEAD
-  require('./json/storyOne.json'),
-  require('./json/storyTwo.json')
-=======
   require("./json/storyOne.json"),
   require("./json/storyTwo.json"),
   require("./json/storyThree.json"),
@@ -13,7 +9,6 @@ const storiesList = [
   require("./json/storyEight.json"),
   require("./json/storyNine.json"),
   require("./json/storyTen.json"),
->>>>>>> origin/jraleman
 ];
 
 export default storiesList;
